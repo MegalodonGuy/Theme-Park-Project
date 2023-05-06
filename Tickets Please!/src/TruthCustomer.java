e8dfe9ff22f86fa86cc0bbe6ba3a70b1ff517fd3
MegalodonGuy <megalodon.boy.42@gmail.com>
2023-05-05 21:20:08 -0400

    added special character class that will be used for special occurrences

--- Tickets Please!/src/TruthCustomer.java
+++ Tickets Please!/src/TruthCustomer.java
@@ -2,13 +2,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class TruthCustomer extends Liar{ // uses inheratance
+/**
+ *  uses inheratance by using many values and methods in the liar class and saves code by using a mian class
+ * uses encapsualtion by encapsualting a sinlge idea between the diffent types of customers
+ */
+public class TruthCustomer extends Liar{ 
+
     public TruthCustomer(){
         
     }
 
  @Override
- public String customerInfo(){
+ public String customerInfo(){ //overides the customer info method in liar so it runs differently than just called normally, this is an example of polymorphism
     return ("Name: "+this.firstName +" "+ this.lastName +" Age: " + this.age + " Ticket ID: " + this.idNum + " Initial Charge: " + this.moneyCharged);
  }
 
